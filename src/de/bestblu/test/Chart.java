860e5d1403e916c1e23b535d498eb8e8f6faedd6
Ostholz <ostholz@gmail.com>
2015-04-23 15:52:51 +0200

    SVG in PDF einbinden, JFreeChart expoertiert als SVG

---
+++ src/de/bestblu/test/Chart.java
@@ -0,0 +1,40 @@
+package de.bestblu.test;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot3D;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+import org.jfree.util.Rotation;
+
+public class Chart {
+
+	public Chart() {
+		
+		
+	}
+	
+	public JFreeChart getChart() {
+		PieDataset dataset = createDataset();
+		JFreeChart chart = createChart(dataset, "Test PieChart");
+		return chart;
+	}
+	
+	private PieDataset createDataset() {
+		DefaultPieDataset result = new DefaultPieDataset();
+		result.setValue("Linux", 29);
+		result.setValue("Mac", 20);
+		result.setValue("Windows", 51);
+		return result;
+	}
+	
+	private JFreeChart createChart(PieDataset dataset, String title) {
+		JFreeChart chart = ChartFactory.createPieChart3D(title, dataset, true, true, false);
+		PiePlot3D plot = (PiePlot3D) chart.getPlot();
+		plot.setStartAngle(290);
+		plot.setDirection(Rotation.CLOCKWISE);
+		plot.setForegroundAlpha(0.9f);
+		return chart;
+	}
+	
+}
